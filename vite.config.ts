2d1b1360e2e25e78e1f452a3db8961df91e66a66
小倔驴的小肥羊 <332383124@qq.com>
2024-04-26 15:06:29 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import dts from 'vite-plugin-dts';
+// import typescript from 'typescript'
 
 // https://vitejs.dev/config/
 export default defineConfig({
